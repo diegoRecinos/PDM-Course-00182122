fea172d83419cd69a34be5e32a0ea59e61244757
recinos <00182122@uca.edu.sv>
2026-05-20 10:42:27 -0600

    vm for screen getmoviebyid

---
+++ ex-mvvm-data-layer/src/main/java/com/pdm0126/ex_mvvm_data_layer_n/screens/MovieDetail/MovieDetail1ViewModel.kt
@@ -0,0 +1,31 @@
+package com.pdm0126.ex_mvvm_data_layer_n.screens.MovieDetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pdm0126.ex_mvvm_data_layer_n.data.repositories.MovieRepository.MovieApiRepository
+import com.pdm0126.ex_mvvm_data_layer_n.data.repositories.MovieRepository.MovieRepository
+import com.pdm0126.ex_mvvm_data_layer_n.model.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MovieDetail1ViewModel : ViewModel() {
+    private val movieRepository: MovieRepository = MovieApiRepository()
+
+    private val _movie = MutableStateFlow<Movie?>(null)
+
+    val movie = _movie.asStateFlow()
+
+    private val _loading = MutableStateFlow<Boolean>(false)
+
+    val loading = _loading.asStateFlow()
+
+    fun loadMovie(movieId: Int) {
+        viewModelScope.launch {
+            _loading.value = true
+            _movie.value = movieRepository.getMovieById(movieId)
+            _loading.value = false
+        }
+    }
+
+}
