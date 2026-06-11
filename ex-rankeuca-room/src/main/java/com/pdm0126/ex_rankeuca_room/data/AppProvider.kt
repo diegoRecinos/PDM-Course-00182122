b759b76370c9158d123439be5b022a42554757ed
recinos <00182122@uca.edu.sv>
2026-06-11 17:40:47 -0600

    added appprovider take priority

--- ex-rankeuca-room/src/main/java/com/pdm0126/ex_rankeuca_room/data/AppProvider.kt
+++ ex-rankeuca-room/src/main/java/com/pdm0126/ex_rankeuca_room/data/AppProvider.kt
@@ -1,2 +1,19 @@
 package com.pdm0126.ex_rankeuca_room.data
 
+import android.content.Context
+import com.pdm0126.ex_rankeuca_room.data.database.AppDatabase
+import com.pdm0126.ex_rankeuca_room.data.repository.OptionRepository
+import com.pdm0126.ex_rankeuca_room.data.repository.OptionRepositoryImpl
+
+class AppProvider(context: Context) {
+
+    private val appDatabase = AppDatabase.getDatabase(context)
+    private val optionDao = appDatabase.optionDao()
+
+    private val optionRepository: OptionRepository =
+        OptionRepositoryImpl(optionDao)
+
+    fun provideOptionRepository(): OptionRepository {
+        return optionRepository
+    }
+}
