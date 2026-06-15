27eb89f5484c4ab0004b7d9826f278795b31a4b5
recinos <00182122@uca.edu.sv>
2026-06-15 16:16:13 -0600

    entity model question

---
+++ ex-rankeuca-room/src/main/java/com/pdm0126/ex_rankeuca_room/data/database/entity/QuestionEntity.kt
@@ -0,0 +1,19 @@
+package com.pdm0126.ex_rankeuca_room.data.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "questions")
+data class QuestionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+)
+
+fun QuestionEntity.toModel(): Question {
+    return Question(
+        id = id,
+        title = title,
+        optionCount = 0,
+    )
+}
---
+++ ex-rankeuca-room/src/main/java/com/pdm0126/ex_rankeuca_room/data/model/Question.kt
@@ -0,0 +1,16 @@
+package com.pdm0126.ex_rankeuca_room.data.model
+
+import com.pdm0126.ex_rankeuca_room.data.database.entity.QuestionEntity
+
+data class Question(
+    val id: Int = 0,
+    val title: String,
+    val optionCount: Int = 0,
+)
+
+fun Question.toEntity(): QuestionEntity {
+    return QuestionEntity(
+        id = id,
+        title = title,
+    )
+}
