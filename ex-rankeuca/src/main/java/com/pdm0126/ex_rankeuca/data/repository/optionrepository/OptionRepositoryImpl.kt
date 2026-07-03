d4e22b304359bb4a00d072019db1cb0010db3664
recinos <00182122@uca.edu.sv>
2026-07-02 18:24:58 -0600

    add: repositoryImpl

--- ex-rankeuca/src/main/java/com/pdm0126/ex_rankeuca/data/repository/optionrepository/OptionRepositoryImpl.kt
+++ ex-rankeuca/src/main/java/com/pdm0126/ex_rankeuca/data/repository/optionrepository/OptionRepositoryImpl.kt
@@ -1,9 +1,17 @@
 package com.pdm0126.ex_rankeuca.data.repository.optionrepository
 
+import com.pdm0126.ex_rankeuca.data.api.KtorClient
+import com.pdm0126.ex_rankeuca.data.api.options.OptionDTO
 import com.pdm0126.ex_rankeuca.data.database.dao.OptionDao
 import com.pdm0126.ex_rankeuca.data.database.entity.toEntity
 import com.pdm0126.ex_rankeuca.data.database.entity.toModel
+import com.pdm0126.ex_rankeuca.data.api.options.toEntity
 import com.pdm0126.ex_rankeuca.data.model.Option
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlin.collections.map
@@ -29,37 +37,58 @@ class OptionRepositoryImpl(
     // sincronizar: van a la API (DTO) -> Room (Entity)
     // El Flow de arriba se encarga de actualizar la UI automáticamente
     override suspend fun refreshOptions() {
-        // En un caso real, aquí decidirías qué questionId usar
-        // o si la API trae el questionId dentro del DTO.
-//        val optionsDto = fetchOptionsFromApi()
-//
-//        // Mapeo directo DTO -> Entity
-//        optionDao.upsertAll(optionsDto.map { it.toEntity(questionId = 1) })
+        try {// En un caso real, aquí decidirías qué questionId usar
+            // o si la API trae el questionId dentro del DTO.
+            val optionsDto = fetchOptionsFromApi()
+
+            // Mapeo directo DTO -> Entity
+            optionDao.upsertAllOptions(optionsDto.map { it.toEntity() })
+
+        } catch (e: Exception) {
+           throw e
+        }
     }
 
+    //mutar API -> luego local refreshOptions()
     override suspend fun voteOption(optionId: Int) {
+        KtorClient.client.post("vote/$optionId")
+        //tras exito, actualizamos Room
         optionDao.incrementVotes(optionId)
     }
 
     override suspend fun resetVotes() {
+        KtorClient.client.post("reset")
+
         optionDao.resetAllVotes()
     }
 
     override suspend fun createOption(name: String, imageUrl: String, questionId: Int) {
-        val option = Option(
-            value = name,
-            imageUrl = imageUrl,
-            questionId = questionId,
-            votes = 0
-        )
-        optionDao.createOption(option.toEntity())
+        //1 llamar API
+
+        val response = KtorClient.client.post("options") {
+            setBody(mapOf("name" to name, "imageUrl" to imageUrl, "questionId" to questionId))
+        }.body<OptionDTO>()
+
+        //guardar resultado real con ID de la API en room
+        optionDao.upsertOption(response.toEntity())
     }
 
     override suspend fun updateOption(option: Option) {
-        optionDao.updateOption(option.toEntity())
+
+        KtorClient.client.post("options/${option.id}")
+
+        optionDao.upsertOption(option.toEntity())
     }
 
     override suspend fun deleteOption(option: Option) {
+
+        KtorClient.client.delete("options/${option.id}")
+
         optionDao.deleteOption(option.toEntity())
     }
+
+    //Helper privado devuelve DTO
+    private suspend fun fetchOptionsFromApi(): List<OptionDTO> =
+        KtorClient.client.get("options").body()
+
 }
